f9bc5c516f6d2605eb938579d4b336364be28296
Jon Cram <jon@webignition.net>
2020-02-10 14:47:34 +0000

    Create ClassDependency line

---
+++ src/Line/ClassDependency.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSource\Line;
+
+use webignition\BasilCompilableSource\SourceInterface;
+
+class ClassDependency implements SourceInterface
+{
+    private const RENDER_PATTERN = 'use %s;';
+
+    private $className;
+    private $alias;
+
+    public function __construct(string $className, ?string $alias = null)
+    {
+        $this->className = $className;
+        $this->alias = $alias;
+    }
+
+    public function render(): string
+    {
+        $content = $this->className;
+
+        if (null !== $this->alias) {
+            $content .= ' as ' . $this->alias;
+        }
+
+        return sprintf(self::RENDER_PATTERN, $content);
+    }
+}
