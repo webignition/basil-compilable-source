bdd833198677e1fbf896fa64cf85756cc1bc97a5
Jon Cram <webignition@gmail.com>
2020-02-10 18:20:13 +0000

    Create Statement line (#33)

---
+++ src/Line/StatementInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSource\Line;
+
+interface StatementInterface extends ExpressionInterface
+{
+}
---
+++ tests/Unit/Line/StatementTest.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSource\Tests\Unit\Line;
+
+use webignition\BasilCompilableSource\Block\ClassDependencyCollection;
+use webignition\BasilCompilableSource\Line\ClassDependency;
+use webignition\BasilCompilableSource\Line\Expression;
+use webignition\BasilCompilableSource\Line\ExpressionInterface;
+use webignition\BasilCompilableSource\Line\Statement;
+use webignition\BasilCompilableSource\Line\StatementInterface;
+use webignition\BasilCompilableSource\Metadata\Metadata;
+use webignition\BasilCompilableSource\Metadata\MetadataInterface;
+use webignition\BasilCompilableSource\VariablePlaceholder;
+use webignition\BasilCompilableSource\VariablePlaceholderCollection;
+
+class StatementTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider createDataProvider
+     */
+    public function testCreate(ExpressionInterface $expression, MetadataInterface $expectedMetadata)
+    {
+        $statement = new Statement($expression);
+
+        $this->assertEquals($expectedMetadata, $statement->getMetadata());
+    }
+
+    public function createDataProvider(): array
+    {
+        return [
+            'variable dependency' => [
+                'expression' => VariablePlaceholder::createDependency('DEPENDENCY'),
+                'expectedMetadata' => new Metadata([
+                    Metadata::KEY_VARIABLE_DEPENDENCIES => VariablePlaceholderCollection::createDependencyCollection([
+                        'DEPENDENCY',
+                    ])
+                ]),
+            ],
+            'variable export' => [
+                'expression' => VariablePlaceholder::createExport('EXPORT'),
+                'expectedMetadata' => new Metadata([
+                    Metadata::KEY_VARIABLE_EXPORTS => VariablePlaceholderCollection::createExportCollection([
+                        'EXPORT',
+                    ])
+                ]),
+            ],
+            'expression with metadata encapsulating variable dependency' => [
+                'expression' => new Expression(
+                    VariablePlaceholder::createDependency('DEPENDENCY'),
+                    new Metadata([
+                        Metadata::KEY_CLASS_DEPENDENCIES => new ClassDependencyCollection([
+                            new ClassDependency(ClassDependency::class),
+                        ]),
+                    ])
+                ),
+                'expectedMetadata' => new Metadata([
+                    Metadata::KEY_CLASS_DEPENDENCIES => new ClassDependencyCollection([
+                        new ClassDependency(ClassDependency::class),
+                    ]),
+                    Metadata::KEY_VARIABLE_DEPENDENCIES => VariablePlaceholderCollection::createDependencyCollection([
+                        'DEPENDENCY',
+                    ])
+                ]),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider renderDataProvider
+     */
+    public function testRender(StatementInterface $statement, string $expectedString)
+    {
+        $this->assertSame($expectedString, $statement->render());
+    }
+
+    public function renderDataProvider(): array
+    {
+        return [
+            'statement encapsulating variable dependency' => [
+                'statement' => new Statement(
+                    VariablePlaceholder::createDependency('DEPENDENCY')
+                ),
+                'expectedString' => '{{ DEPENDENCY }};',
+            ],
+            'statement encapsulating variable export' => [
+                'statement' => new Statement(
+                    VariablePlaceholder::createExport('EXPORT')
+                ),
+                'expectedString' => '{{ EXPORT }};',
+            ],
+            'statement encapsulating expression with metadata encapsulating variable dependency' => [
+                'statement' => new Statement(
+                    new Expression(
+                        VariablePlaceholder::createDependency('DEPENDENCY'),
+                        new Metadata([
+                            Metadata::KEY_CLASS_DEPENDENCIES => new ClassDependencyCollection([
+                                new ClassDependency(ClassDependency::class),
+                            ]),
+                        ])
+                    )
+                ),
+                'expectedString' => '{{ DEPENDENCY }};',
+            ],
+        ];
+    }
+}
