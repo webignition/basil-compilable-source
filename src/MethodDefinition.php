1a4bd62e377cc49431e8bb8d9ed5f742d12f3587
Jon Cram <jon@webignition.net>
2020-02-11 16:46:25 +0000

    Create MethodDefinition

---
+++ src/MethodDefinition.php
@@ -0,0 +1,208 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSource;
+
+use webignition\BasilCompilableSource\Block\CodeBlockInterface;
+use webignition\BasilCompilableSource\Block\DocBlock;
+use webignition\BasilCompilableSource\Metadata\MetadataInterface;
+
+class MethodDefinition implements MethodDefinitionInterface
+{
+    private const RENDER_TEMPLATE = <<<'EOD'
+%s
+{
+%s
+}
+EOD;
+
+    public const VISIBILITY_PUBLIC = 'public';
+    public const VISIBILITY_PROTECTED = 'protected';
+    public const VISIBILITY_PRIVATE = 'private';
+
+    private $visibility = self::VISIBILITY_PUBLIC;
+
+    /**
+     * @var string|null
+     */
+    private $returnType = null;
+    private $name;
+    private $codeBlock;
+
+    /**
+     * @var string[]
+     */
+    private $arguments = [];
+    private $isStatic = false;
+
+    /**
+     * @var DocBlock|null
+     */
+    private $docBlock;
+
+    /**
+     * @param string $name
+     * @param CodeBlockInterface $codeBlock
+     * @param string[] $arguments
+     */
+    public function __construct(string $name, CodeBlockInterface $codeBlock, array $arguments = [])
+    {
+        $this->visibility = self::VISIBILITY_PUBLIC;
+        $this->returnType = null;
+        $this->name = $name;
+        $this->codeBlock = $codeBlock;
+        $this->arguments = $arguments;
+        $this->isStatic = false;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getCodeBlock(): CodeBlockInterface
+    {
+        return $this->codeBlock;
+    }
+
+    public function getArguments(): array
+    {
+        return $this->arguments;
+    }
+
+    public function addLine(LineInterface $statement): void
+    {
+        $this->codeBlock->addLine($statement);
+    }
+
+    public function getLines(): \ArrayIterator
+    {
+        return $this->codeBlock->getLines();
+    }
+
+    public function getMetadata(): MetadataInterface
+    {
+        return $this->codeBlock->getMetadata();
+    }
+
+    public function setPublic(): void
+    {
+        $this->visibility = self::VISIBILITY_PUBLIC;
+    }
+
+    public function setProtected(): void
+    {
+        $this->visibility = self::VISIBILITY_PROTECTED;
+    }
+
+    public function setPrivate(): void
+    {
+        $this->visibility = self::VISIBILITY_PRIVATE;
+    }
+
+    public function getReturnType(): ?string
+    {
+        return $this->returnType;
+    }
+
+    public function setReturnType(?string $returnType): void
+    {
+        $this->returnType = $returnType;
+    }
+
+    public function setStatic(): void
+    {
+        $this->isStatic = true;
+    }
+
+    public function isStatic(): bool
+    {
+        return $this->isStatic;
+    }
+
+    public function getVisibility(): string
+    {
+        return $this->visibility;
+    }
+
+    public function setDocBlock(DocBlock $docBlock): void
+    {
+        $this->docBlock = $docBlock;
+    }
+
+    public function getDocBlock(): ?DocBlock
+    {
+        return $this->docBlock;
+    }
+
+    public function render(): string
+    {
+        $signature = $this->createSignature();
+
+        $lines = $this->codeBlock->render();
+        $lines = $this->indent($lines);
+        $lines = rtrim($lines, "\n");
+
+        $content = sprintf(self::RENDER_TEMPLATE, $signature, $lines);
+
+        if ($this->docBlock instanceof DocBlock) {
+            $content = $this->docBlock->render() . "\n" . $content;
+        }
+
+        return $content;
+    }
+
+    private function createSignature(): string
+    {
+        $signature = $this->getVisibility() . ' ';
+
+        if ($this->isStatic()) {
+            $signature .= 'static ';
+        }
+
+        $arguments = $this->createSignatureArguments($this->getArguments());
+        $signature .= 'function ' . $this->getName() . '(' . $arguments . ')';
+
+        $returnType = $this->getReturnType();
+
+        if (null !== $returnType) {
+            $signature .= ': ' . $returnType;
+        }
+
+        return $signature;
+    }
+
+    /**
+     * @param array<string, string> $argumentNames
+     *
+     * @return string
+     */
+    private function createSignatureArguments(array $argumentNames): string
+    {
+        $arguments = $argumentNames;
+
+        array_walk($arguments, function (&$argument) {
+            $argument = '$' . $argument;
+        });
+
+        return implode(', ', $arguments);
+    }
+
+    private function indent(string $content): string
+    {
+        if ('' === $content) {
+            return '';
+        }
+
+        $lines = explode("\n", $content);
+
+        array_walk($lines, function (&$line) {
+            if ('' !== $line) {
+                $line = '    ' . $line;
+            }
+        });
+
+        return implode("\n", $lines);
+    }
+}
---
+++ tests/Unit/MethodDefinitionTest.php
@@ -0,0 +1,363 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSource\Tests\Unit;
+
+use webignition\BasilCompilableSource\Block\CodeBlock;
+use webignition\BasilCompilableSource\Block\CodeBlockInterface;
+use webignition\BasilCompilableSource\Block\DocBlock;
+use webignition\BasilCompilableSource\Line\EmptyLine;
+use webignition\BasilCompilableSource\Line\MethodInvocation\MethodInvocation;
+use webignition\BasilCompilableSource\Line\MethodInvocation\ObjectMethodInvocation;
+use webignition\BasilCompilableSource\Line\SingleLineComment;
+use webignition\BasilCompilableSource\Line\Statement\AssignmentStatement;
+use webignition\BasilCompilableSource\Line\Statement\Statement;
+use webignition\BasilCompilableSource\LineInterface;
+use webignition\BasilCompilableSource\Metadata\Metadata;
+use webignition\BasilCompilableSource\Metadata\MetadataInterface;
+use webignition\BasilCompilableSource\MethodDefinition;
+use webignition\BasilCompilableSource\MethodDefinitionInterface;
+use webignition\BasilCompilableSource\VariablePlaceholder;
+use webignition\BasilCompilableSource\VariablePlaceholderCollection;
+
+class MethodDefinitionTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider createDataProvider
+     *
+     * @param string $name
+     * @param CodeBlockInterface $codeBlock
+     * @param string[] $arguments
+     */
+    public function testCreate(string $name, CodeBlockInterface $codeBlock, array $arguments = [])
+    {
+        $methodDefinition = new MethodDefinition($name, $codeBlock, $arguments);
+
+        $this->assertSame($name, $methodDefinition->getName());
+        $this->assertSame($codeBlock, $methodDefinition->getCodeBlock());
+        $this->assertSame($arguments, $methodDefinition->getArguments());
+        $this->assertsame(MethodDefinition::VISIBILITY_PUBLIC, $methodDefinition->getVisibility());
+        $this->assertNull($methodDefinition->getReturnType());
+        $this->assertFalse($methodDefinition->isStatic());
+        $this->assertNull($methodDefinition->getDocBlock());
+    }
+
+    public function createDataProvider(): array
+    {
+        return [
+            'no arguments' => [
+                'name' => 'noArguments',
+                'codeBlock' => new CodeBlock(),
+            ],
+            'empty arguments' => [
+                'name' => 'emptyArguments',
+                'codeBlock' => new CodeBlock(),
+                'arguments' => [],
+            ],
+            'has arguments' => [
+                'name' => 'hasArguments',
+                'codeBlock' => new CodeBlock(),
+                'arguments' => [
+                    'arg1',
+                    'arg2',
+                ],
+            ],
+        ];
+    }
+
+    public function testAddLine()
+    {
+        $methodDefinition = new MethodDefinition('name', new CodeBlock());
+        $this->assertEquals([], $this->getIteratorLines($methodDefinition->getLines()));
+
+        $methodDefinition->addLine(new EmptyLine());
+        $this->assertEquals(
+            [
+                new EmptyLine(),
+            ],
+            $this->getIteratorLines($methodDefinition->getLines())
+        );
+
+        $methodDefinition->addLine(new SingleLineComment('comment'));
+        $this->assertEquals(
+            [
+                new EmptyLine(),
+                new SingleLineComment('comment'),
+            ],
+            $this->getIteratorLines($methodDefinition->getLines())
+        );
+    }
+
+    /**
+     * @dataProvider getMetadataDataProvider
+     */
+    public function testGetMetadata(MethodDefinitionInterface $methodDefinition, MetadataInterface $expectedMetadata)
+    {
+        $this->assertEquals($expectedMetadata, $methodDefinition->getMetadata());
+    }
+
+    public function getMetadataDataProvider(): array
+    {
+        return [
+            'empty' => [
+                'methodDefinition' => new MethodDefinition('name', new CodeBlock()),
+                'expectedMetadata' => new Metadata(),
+            ],
+            'lines without metadata' => [
+                'methodDefinition' => new MethodDefinition('name', new CodeBlock([
+                    new EmptyLine(),
+                    new SingleLineComment('single line comment'),
+                ])),
+                'expectedMetadata' => new Metadata(),
+            ],
+            'lines with metadata' => [
+                'methodDefinition' => new MethodDefinition('name', new CodeBlock([
+                    new Statement(
+                        new ObjectMethodInvocation(
+                            VariablePlaceholder::createDependency('DEPENDENCY'),
+                            'methodName'
+                        )
+                    ),
+                    new AssignmentStatement(
+                        VariablePlaceholder::createExport('PLACEHOLDER'),
+                        new MethodInvocation('methodName')
+                    ),
+                ])),
+                'expectedMetadata' => new Metadata([
+                    Metadata::KEY_VARIABLE_DEPENDENCIES => VariablePlaceholderCollection::createDependencyCollection([
+                        'DEPENDENCY',
+                    ]),
+                    Metadata::KEY_VARIABLE_EXPORTS => VariablePlaceholderCollection::createExportCollection([
+                        'PLACEHOLDER',
+                    ]),
+                ]),
+            ],
+        ];
+    }
+
+    public function testVisibility()
+    {
+        $methodDefinition = new MethodDefinition('name', new CodeBlock());
+        $this->assertSame(MethodDefinition::VISIBILITY_PUBLIC, $methodDefinition->getVisibility());
+
+        $methodDefinition->setProtected();
+        $this->assertSame(MethodDefinition::VISIBILITY_PROTECTED, $methodDefinition->getVisibility());
+
+        $methodDefinition->setPrivate();
+        $this->assertSame(MethodDefinition::VISIBILITY_PRIVATE, $methodDefinition->getVisibility());
+
+        $methodDefinition->setPublic();
+        $this->assertSame(MethodDefinition::VISIBILITY_PUBLIC, $methodDefinition->getVisibility());
+    }
+
+    public function testSetReturnType()
+    {
+        $methodDefinition = new MethodDefinition('name', new CodeBlock());
+        $this->assertNull($methodDefinition->getReturnType());
+
+        $methodDefinition->setReturnType('string');
+        $this->assertSame('string', $methodDefinition->getReturnType());
+
+        $methodDefinition->setReturnType('void');
+        $this->assertSame('void', $methodDefinition->getReturnType());
+
+        $methodDefinition->setReturnType(null);
+        $this->assertNull($methodDefinition->getReturnType());
+    }
+
+    public function testIsStatic()
+    {
+        $methodDefinition = new MethodDefinition('name', new CodeBlock());
+        $this->assertFalse($methodDefinition->isStatic());
+
+        $methodDefinition->setStatic();
+        $this->assertTrue($methodDefinition->isStatic());
+    }
+
+    public function testSetDocBlock()
+    {
+        $methodDefinition = new MethodDefinition('name', new CodeBlock());
+        $this->assertNull($methodDefinition->getDocBlock());
+
+        $docBlock = new DocBlock();
+        $methodDefinition->setDocBlock($docBlock);
+        $this->assertSame($docBlock, $methodDefinition->getDocBlock());
+    }
+
+    /**
+     * @dataProvider renderDataProvider
+     */
+    public function testRender(MethodDefinitionInterface $methodDefinition, string $expectedString)
+    {
+        $this->assertSame($expectedString, $methodDefinition->render());
+    }
+
+    public function renderDataProvider(): array
+    {
+        $emptyProtectedMethod = new MethodDefinition('emptyProtectedMethod', new CodeBlock());
+        $emptyProtectedMethod->setProtected();
+
+        $emptyPrivateMethod = new MethodDefinition('emptyPrivateMethod', new CodeBlock());
+        $emptyPrivateMethod->setPrivate();
+
+        $emptyMethodWithReturnType = new MethodDefinition('emptyPublicMethodWithReturnType', new CodeBlock());
+        $emptyMethodWithReturnType->setReturnType('string');
+
+        $emptyPublicStaticMethod = new MethodDefinition('emptyPublicStaticMethod', new CodeBlock());
+        $emptyPublicStaticMethod->setStatic();
+
+        return [
+            'public, no arguments, no return type, no lines' => [
+                'methodDefinition' => new MethodDefinition('emptyPublicMethod', new CodeBlock()),
+                'expectedString' =>
+                    'public function emptyPublicMethod()' . "\n" .
+                    '{' . "\n\n" .
+                    '}'
+            ],
+            'protected, no arguments, no return type, no lines' => [
+                'methodDefinition' => $emptyProtectedMethod,
+                'expectedString' =>
+                    'protected function emptyProtectedMethod()' . "\n" .
+                    '{' . "\n\n" .
+                    '}'
+            ],
+            'private, no arguments, no return type, no lines' => [
+                'methodDefinition' => $emptyPrivateMethod,
+                'expectedString' =>
+                    'private function emptyPrivateMethod()' . "\n" .
+                    '{' . "\n\n" .
+                    '}'
+            ],
+            'public, has arguments, no return type, no lines' => [
+                'methodDefinition' => new MethodDefinition('emptyPublicMethod', new CodeBlock(), [
+                    'arg1',
+                    'arg2',
+                    'arg3',
+                ]),
+                'expectedString' =>
+                    'public function emptyPublicMethod($arg1, $arg2, $arg3)' . "\n" .
+                    '{' . "\n\n" .
+                    '}'
+            ],
+            'public, no arguments, has return type, no lines' => [
+                'methodDefinition' => $emptyMethodWithReturnType,
+                'expectedString' =>
+                    'public function emptyPublicMethodWithReturnType(): string' . "\n" .
+                    '{' . "\n\n" .
+                    '}'
+            ],
+            'public, has arguments, no return type, has lines' => [
+                'methodDefinition' => new MethodDefinition(
+                    'nameOfMethod',
+                    new CodeBlock([
+                        new SingleLineComment('Assign object method call to VALUE'),
+                        new EmptyLine(),
+                        new AssignmentStatement(
+                            VariablePlaceholder::createExport('VALUE'),
+                            new ObjectMethodInvocation(
+                                VariablePlaceholder::createDependency('OBJECT'),
+                                'methodName',
+                                [
+                                    '$x',
+                                    '$y',
+                                ]
+                            )
+                        ),
+                    ]),
+                    ['x', 'y']
+                ),
+                'expectedString' =>
+                    'public function nameOfMethod($x, $y)' . "\n" .
+                    '{' . "\n" .
+                    '    // Assign object method call to VALUE' . "\n" .
+                    "\n" .
+                    '    {{ VALUE }} = {{ OBJECT }}->methodName($x, $y);' . "\n" .
+                    '}'
+            ],
+            'public, has arguments, no return type, has lines with trailing newline' => [
+                'methodDefinition' => new MethodDefinition(
+                    'nameOfMethod',
+                    new CodeBlock([
+                        new SingleLineComment('comment'),
+                        new EmptyLine(),
+                    ]),
+                    ['x', 'y']
+                ),
+                'expectedString' =>
+                    'public function nameOfMethod($x, $y)' . "\n" .
+                    '{' . "\n" .
+                    '    // comment' . "\n" .
+                    '}'
+            ],
+            'public static, no arguments, no return type, no lines' => [
+                'methodDefinition' => $emptyPublicStaticMethod,
+                'expectedString' =>
+                    'public static function emptyPublicStaticMethod()' . "\n" .
+                    '{' . "\n\n" .
+                    '}'
+            ],
+            'public, has arguments, no return type, has lines, with docblock' => [
+                'methodDefinition' => $this->createMethodDefinitionWithDocBlock(
+                    new MethodDefinition(
+                        'nameOfMethod',
+                        new CodeBlock([
+                            new SingleLineComment('Assign object method call to VALUE'),
+                            new EmptyLine(),
+                            new AssignmentStatement(
+                                VariablePlaceholder::createExport('VALUE'),
+                                new ObjectMethodInvocation(
+                                    VariablePlaceholder::createDependency('OBJECT'),
+                                    'methodName',
+                                    [
+                                        '$x',
+                                        '$y',
+                                    ]
+                                )
+                            ),
+                        ]),
+                        ['x', 'y']
+                    ),
+                    new DocBlock([
+                        new SingleLineComment('@dataProvider nameOfMethodDataProvider'),
+                    ])
+                ),
+                'expectedString' =>
+                    '/**' . "\n" .
+                    ' * @dataProvider nameOfMethodDataProvider' . "\n" .
+                    ' */' . "\n" .
+                    'public function nameOfMethod($x, $y)' . "\n" .
+                    '{' . "\n" .
+                    '    // Assign object method call to VALUE' . "\n" .
+                    "\n" .
+                    '    {{ VALUE }} = {{ OBJECT }}->methodName($x, $y);' . "\n" .
+                    '}'
+            ],
+        ];
+    }
+
+    /**
+     * @param \ArrayIterator<int, LineInterface> $iterator
+     *
+     * @return LineInterface[]
+     */
+    private function getIteratorLines(\ArrayIterator $iterator): array
+    {
+        $lines = [];
+
+        foreach ($iterator as $line) {
+            $lines[] = $line;
+        }
+
+        return $lines;
+    }
+
+    private function createMethodDefinitionWithDocBlock(
+        MethodDefinition $methodDefinition,
+        DocBlock $docBlock
+    ): MethodDefinitionInterface {
+        $methodDefinition->setDocBlock($docBlock);
+
+        return $methodDefinition;
+    }
+}
