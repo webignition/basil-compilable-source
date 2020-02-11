d8eba500c2bc13a209ac296c1fbeab587f04c789
Jon Cram <webignition@gmail.com>
2020-02-11 16:58:15 +0000

    Create MethodDefinition (#61)

---
+++ src/MethodDefinitionInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSource;
+
+use webignition\BasilCompilableSource\Block\CodeBlockInterface;
+use webignition\BasilCompilableSource\Block\DocBlock;
+
+interface MethodDefinitionInterface extends CodeBlockInterface
+{
+    /**
+     * @return string[]
+     */
+    public function getArguments(): array;
+    public function getName(): string;
+    public function getReturnType(): ?string;
+    public function getVisibility(): string;
+    public function getCodeBlock(): CodeBlockInterface;
+    public function getDocBlock(): ?DocBlock;
+    public function isStatic(): bool;
+}
