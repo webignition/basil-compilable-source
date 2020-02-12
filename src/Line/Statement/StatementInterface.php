297e0d5ac86d3c23d35c203a5c4a5cb81f7de18b
Jon Cram <webignition@gmail.com>
2020-02-12 15:43:21 +0000

    Statement is not an Expression (#75)

--- src/Line/Statement/StatementInterface.php
+++ src/Line/Statement/StatementInterface.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace webignition\BasilCompilableSource\Line\Statement;
 
 use webignition\BasilCompilableSource\Line\ExpressionInterface;
+use webignition\BasilCompilableSource\LineInterface;
+use webignition\BasilCompilableSource\Metadata\MetadataInterface;
 
-interface StatementInterface extends ExpressionInterface
+interface StatementInterface extends LineInterface
 {
     public function getExpression(): ExpressionInterface;
+    public function getMetadata(): MetadataInterface;
 }
