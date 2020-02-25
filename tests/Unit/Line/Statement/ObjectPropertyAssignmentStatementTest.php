aa1e97f708d1cdafaaaa2eb3c33d9a49d29f0926
Jon Cram <webignition@gmail.com>
2020-02-25 21:02:33 +0000

    Create ObjectPropertyAssignmentStatement (#106)

---
+++ tests/Unit/Line/Statement/ObjectPropertyAssignmentStatementTest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSource\Tests\Unit\Line\Statement;
+
+use webignition\BasilCompilableSource\Line\ObjectPropertyAccessExpression;
+use webignition\BasilCompilableSource\Line\Statement\AssignmentStatementInterface;
+use webignition\BasilCompilableSource\Line\ExpressionInterface;
+use webignition\BasilCompilableSource\Line\Statement\ObjectPropertyAssignmentStatement;
+use webignition\BasilCompilableSource\VariablePlaceholder;
+
+class ObjectPropertyAssignmentStatementTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider createDataProvider
+     */
+    public function testCreate(
+        ObjectPropertyAccessExpression $placeholder,
+        ExpressionInterface $expression,
+        ObjectPropertyAccessExpression $expectedPlaceholder
+    ) {
+        $statement = new ObjectPropertyAssignmentStatement($placeholder, $expression);
+
+        $this->assertEquals($expectedPlaceholder, $statement->getVariablePlaceholder());
+        $this->assertSame($expression, $statement->getExpression());
+    }
+
+    public function createDataProvider(): array
+    {
+        return [
+            'no castTo' => [
+                'placeholder' => new ObjectPropertyAccessExpression(
+                    VariablePlaceholder::createExport('DEPENDENCY'),
+                    'propertyName'
+                ),
+                'expression' => VariablePlaceholder::createDependency('DEPENDENCY'),
+                'expectedPlaceholder' => new ObjectPropertyAccessExpression(
+                    VariablePlaceholder::createExport('DEPENDENCY'),
+                    'propertyName'
+                ),
+            ],
+            'has castTo' => [
+                'placeholder' => new ObjectPropertyAccessExpression(
+                    VariablePlaceholder::createExport('DEPENDENCY'),
+                    'propertyName',
+                    'string'
+                ),
+                'expression' => VariablePlaceholder::createDependency('DEPENDENCY'),
+                'expectedPlaceholder' => new ObjectPropertyAccessExpression(
+                    VariablePlaceholder::createExport('DEPENDENCY'),
+                    'propertyName'
+                ),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider renderDataProvider
+     */
+    public function testRender(AssignmentStatementInterface $statement, string $expectedString)
+    {
+        $this->assertSame($expectedString, $statement->render());
+    }
+
+    public function renderDataProvider(): array
+    {
+        return [
+            'no castTo' => [
+                'statement' => new ObjectPropertyAssignmentStatement(
+                    new ObjectPropertyAccessExpression(
+                        VariablePlaceholder::createExport('PLACEHOLDER'),
+                        'propertyName'
+                    ),
+                    VariablePlaceholder::createDependency('DEPENDENCY')
+                ),
+                'expectedString' => '{{ PLACEHOLDER }}->propertyName = {{ DEPENDENCY }};',
+            ],
+            'has castTo' => [
+                'statement' => new ObjectPropertyAssignmentStatement(
+                    new ObjectPropertyAccessExpression(
+                        VariablePlaceholder::createExport('PLACEHOLDER'),
+                        'propertyName',
+                        'string'
+                    ),
+                    VariablePlaceholder::createDependency('DEPENDENCY')
+                ),
+                'expectedString' => '{{ PLACEHOLDER }}->propertyName = {{ DEPENDENCY }};',
+            ],
+        ];
+    }
+}
