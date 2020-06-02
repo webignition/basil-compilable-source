2145f99a7139817efd604cb9a694b83547df74c2
Jon Cram <webignition@gmail.com>
2020-06-02 17:44:59 +0100

    Render ClosureExpression without calling CodeBlock::getLines() (#128)

--- src/Line/ClosureExpression.php
+++ src/Line/ClosureExpression.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 namespace webignition\BasilCompilableSource\Line;
 
 use webignition\BasilCompilableSource\Block\CodeBlockInterface;
-use webignition\BasilCompilableSource\Line\Statement\StatementInterface;
 use webignition\BasilCompilableSource\Metadata\MetadataInterface;
 
 class ClosureExpression extends AbstractExpression
 {
     private const RENDER_TEMPLATE = <<<'EOD'
 (function () {
-%s%s
+%s
 })()
 EOD;
 
@@ -37,34 +36,9 @@ EOD;
 
     public function render(): string
     {
-        $codeBlockLines = $this->codeBlock->getLines();
-
-        $renderedBodyStatements = '';
-        $renderedFinalStatement = '';
-
-        if (count($codeBlockLines) > 0) {
-            $finalStatement = array_pop($codeBlockLines);
-            $bodyStatements = $codeBlockLines;
-
-            $renderedBodyStatements = (string) array_reduce(
-                $bodyStatements,
-                function (?string $content, StatementInterface $statement) {
-                    return $content . $statement->render() . "\n";
-                }
-            );
-
-            $renderedBodyStatements = $this->indent($renderedBodyStatements);
-            $renderedFinalStatement = $this->indent($finalStatement->render());
-        }
-
-        if ('' !== $renderedBodyStatements) {
-            $renderedBodyStatements .= "\n";
-        }
-
         return sprintf(
             self::RENDER_TEMPLATE,
-            $renderedBodyStatements,
-            $renderedFinalStatement
+            $this->indent($this->codeBlock->render())
         );
     }
 
