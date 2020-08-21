dc585df4517ac059d0ceeaa800dea4e5ad50b710
Jon Cram <webignition@gmail.com>
2020-08-21 16:51:31 +0100

    Fix CS

--- src/TypeDeclaration/ObjectTypeDeclarationCollection.php
+++ src/TypeDeclaration/ObjectTypeDeclarationCollection.php
@@ -54,7 +54,7 @@ class ObjectTypeDeclarationCollection implements TypeDeclarationCollectionInterf
                 return 0;
             }
 
-            return ($a < $b) ? -1 : 1;
+            return $a < $b ? -1 : 1;
         });
 
         return implode(' | ', $renderedDeclarations);
