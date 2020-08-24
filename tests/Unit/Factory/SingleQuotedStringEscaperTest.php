6426eca9a3b26fdcb9c93d5360ba9516237cf2bb
Jon Cram <webignition@gmail.com>
2020-08-24 10:38:30 +0100

    Create SingleQuotedStringEscaper (#218)

---
+++ tests/Unit/Factory/SingleQuotedStringEscaperTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilableSource\Tests\Unit\Factory;
+
+use webignition\BasilCompilableSource\Factory\SingleQuotedStringEscaper;
+
+class SingleQuotedStringEscaperTest extends \PHPUnit\Framework\TestCase
+{
+    private SingleQuotedStringEscaper $escaper;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->escaper = SingleQuotedStringEscaper::create();
+    }
+
+    /**
+     * @dataProvider escapeDataProvider
+     */
+    public function testEscape(string $string, string $expectedEscapedString)
+    {
+        $this->assertEquals($expectedEscapedString, $this->escaper->escape($string));
+    }
+
+    public function escapeDataProvider(): array
+    {
+        return [
+            'no single quotes' => [
+                'string' => 'value',
+                'expectedEscapedString' => 'value',
+            ],
+            'encapsulated in single quotes' => [
+                'string' => "'value'",
+                'expectedEscapedString' => "\'value\'",
+            ],
+            'contains single quotes' => [
+                'string' => "va'lu'e",
+                'expectedEscapedString' => "va\'lu\'e",
+            ],
+            'escaped single quotes' => [
+                'string' => "\'value\'",
+                'expectedEscapedString' => "\\\\\'value\\\\\'",
+            ],
+            'contains single quotes for json_encoded data' => [
+                'string' => '"va\\\'l\\\'ue"',
+                'expectedEscapedString' => '"va\\\\\\\'l\\\\\\\'ue"',
+            ],
+        ];
+    }
+}
