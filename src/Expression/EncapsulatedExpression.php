eda7bde025c0d1c66d24b1c9dd370f087665a4ee
Jon Cram <webignition@gmail.com>
2020-10-09 14:45:42 +0100

    Use named placeholders in ComparisonExpression, EncapsulatedExpression, ObjectPropertyAccessExpression

--- src/Expression/EncapsulatedExpression.php
+++ src/Expression/EncapsulatedExpression.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace webignition\BasilCompilableSource\Expression;
 
+use webignition\BasilCompilableSource\RenderFromTemplateTrait;
+
 class EncapsulatedExpression extends AbstractExpression
 {
+    use RenderFromTemplateTrait;
+
+    private const RENDER_TEMPLATE = '({{ expression }})';
+
     private ExpressionInterface $expression;
 
     public function __construct(ExpressionInterface $expression)
@@ -15,8 +21,15 @@ class EncapsulatedExpression extends AbstractExpression
         parent::__construct($expression->getMetadata());
     }
 
-    public function render(): string
+    protected function getRenderTemplate(): string
     {
-        return '(' . $this->expression->render() . ')';
+        return self::RENDER_TEMPLATE;
+    }
+
+    protected function getRenderContext(): array
+    {
+        return [
+            'expression' => $this->expression->render(),
+        ];
     }
 }
