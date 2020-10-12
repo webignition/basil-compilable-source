ecc878306cc0391a2245b0df21f1fdc34d5224be
Jon Cram <webignition@gmail.com>
2020-10-12 11:14:14 +0100

    Tidy up AssignmentStatement, ObjectPropertyAssignmentStatement

--- src/Statement/AssignmentStatement.php
+++ src/Statement/AssignmentStatement.php
@@ -5,19 +5,16 @@ declare(strict_types=1);
 namespace webignition\BasilCompilableSource\Statement;
 
 use webignition\BasilCompilableSource\Expression\ExpressionInterface;
-use webignition\BasilCompilableSource\Metadata\MetadataInterface;
 
-class AssignmentStatement implements AssignmentStatementInterface
+class AssignmentStatement extends AbstractAssignmentStatement implements AssignmentStatementInterface
 {
-    private const RENDER_PATTERN = '%s = %s';
-
     private ExpressionInterface $variableDependency;
-    private Statement $valueStatement;
 
     private function __construct(ExpressionInterface $variableDependency, Statement $valueStatement)
     {
+        parent::__construct($valueStatement);
+
         $this->variableDependency = $variableDependency;
-        $this->valueStatement = $valueStatement;
     }
 
     public static function createFromExpression(
@@ -33,27 +30,5 @@ class AssignmentStatement implements AssignmentStatementInterface
     public function getVariableDependency(): ExpressionInterface
     {
         return $this->variableDependency;
-    }
-
-    public function getMetadata(): MetadataInterface
-    {
-        $metadata = $this->variableDependency->getMetadata();
-        $metadata = $metadata->merge($this->valueStatement->getMetadata());
-
-        return $metadata;
-    }
-
-    public function getExpression(): ExpressionInterface
-    {
-        return $this->valueStatement->getExpression();
-    }
-
-    public function render(): string
-    {
-        return sprintf(
-            self::RENDER_PATTERN,
-            $this->variableDependency->render(),
-            $this->valueStatement->render()
-        );
     }
 }
--- src/Statement/ObjectPropertyAssignmentStatement.php
+++ src/Statement/ObjectPropertyAssignmentStatement.php
@@ -6,19 +6,16 @@ namespace webignition\BasilCompilableSource\Statement;
 
 use webignition\BasilCompilableSource\Expression\ExpressionInterface;
 use webignition\BasilCompilableSource\Expression\ObjectPropertyAccessExpression;
-use webignition\BasilCompilableSource\Metadata\MetadataInterface;
 
-class ObjectPropertyAssignmentStatement implements AssignmentStatementInterface
+class ObjectPropertyAssignmentStatement extends AbstractAssignmentStatement implements AssignmentStatementInterface
 {
-    private const RENDER_PATTERN = '%s = %s';
-
     private ObjectPropertyAccessExpression $accessExpression;
-    private Statement $valueStatement;
 
     private function __construct(ObjectPropertyAccessExpression $accessExpression, Statement $valueStatement)
     {
+        parent::__construct($valueStatement);
+
         $this->accessExpression = $accessExpression;
-        $this->valueStatement = $valueStatement;
     }
 
     public static function createFromExpression(
@@ -34,27 +31,5 @@ class ObjectPropertyAssignmentStatement implements AssignmentStatementInterface
     public function getVariableDependency(): ExpressionInterface
     {
         return $this->accessExpression;
-    }
-
-    public function getMetadata(): MetadataInterface
-    {
-        $metadata = $this->getVariableDependency()->getMetadata();
-        $metadata = $metadata->merge($this->valueStatement->getMetadata());
-
-        return $metadata;
-    }
-
-    public function getExpression(): ExpressionInterface
-    {
-        return $this->valueStatement->getExpression();
-    }
-
-    public function render(): string
-    {
-        return sprintf(
-            self::RENDER_PATTERN,
-            $this->getVariableDependency()->render(),
-            $this->valueStatement->render()
-        );
     }
 }
