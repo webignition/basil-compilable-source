333b63f68fe2d4676bce711b96c5ff5b1230951e
Jon Cram <webignition@gmail.com>
2020-10-21 17:44:21 +0100

    Remove VariableName::getResolvable()

--- src/VariableName.php
+++ src/VariableName.php
@@ -7,14 +7,10 @@ namespace webignition\BasilCompilableSource;
 use webignition\BasilCompilableSource\Expression\ExpressionInterface;
 use webignition\BasilCompilableSource\Metadata\Metadata;
 use webignition\BasilCompilableSource\Metadata\MetadataInterface;
-use webignition\StubbleResolvable\Resolvable;
-use webignition\StubbleResolvable\ResolvableInterface;
 
 class VariableName implements ExpressionInterface, VariablePlaceholderInterface
 {
     use RenderFromTemplateTrait;
-
-    private const RENDER_TEMPLATE = '${{ name }}';
 
     private string $name;
     private MetadataInterface $metadata;
@@ -33,16 +29,6 @@ class VariableName implements ExpressionInterface, VariablePlaceholderInterface
     public function getMetadata(): MetadataInterface
     {
         return $this->metadata;
-    }
-
-    public function getResolvable(): ResolvableInterface
-    {
-        return new Resolvable(
-            self::RENDER_TEMPLATE,
-            [
-                'name' => $this->name,
-            ]
-        );
     }
 
     public function __toString(): string
