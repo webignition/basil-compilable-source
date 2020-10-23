48cb6aaab043a951b83fc24c8e7964bf7274cbc1
Jon Cram <webignition@gmail.com>
2020-10-23 11:41:14 +0100

    EncapsulatedExpression, LiteralExpression does not extend AbstractExpression

--- src/Expression/LiteralExpression.php
+++ src/Expression/LiteralExpression.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace webignition\BasilCompilableSource\Expression;
 
+use webignition\BasilCompilableSource\Metadata\Metadata;
+use webignition\BasilCompilableSource\Metadata\MetadataInterface;
 use webignition\BasilCompilableSource\RenderTrait;
 
-class LiteralExpression extends AbstractExpression
+class LiteralExpression implements ExpressionInterface
 {
     use RenderTrait;
 
@@ -14,13 +16,16 @@ class LiteralExpression extends AbstractExpression
 
     public function __construct(string $content)
     {
-        parent::__construct();
-
         $this->content = $content;
     }
 
     public function __toString(): string
     {
         return $this->content;
+    }
+
+    public function getMetadata(): MetadataInterface
+    {
+        return new Metadata();
     }
 }
