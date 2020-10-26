f38e25c5c607c0cda0ae00169bc9e752d414375c
Jon Cram <webignition@gmail.com>
2020-10-26 11:59:09 +0000

    Remove usage of ResolvableProviderInterface (#377)

--- src/RenderTrait.php
+++ src/RenderTrait.php
@@ -8,26 +8,19 @@ use webignition\Stubble\DeciderFactory;
 use webignition\Stubble\UnresolvedVariableFinder;
 use webignition\Stubble\VariableResolver;
 use webignition\StubbleResolvable\ResolvableInterface;
-use webignition\StubbleResolvable\ResolvableProviderInterface;
 
 trait RenderTrait
 {
     public function render(): string
     {
-        if ($this instanceof ResolvableInterface || $this instanceof ResolvableProviderInterface) {
+        if ($this instanceof ResolvableInterface) {
             $resolver = new VariableResolver(
                 new UnresolvedVariableFinder([
                     DeciderFactory::createAllowAllDecider()
                 ])
             );
 
-            if ($this instanceof ResolvableInterface) {
-                return $resolver->resolveAndIgnoreUnresolvedVariables($this);
-            }
-
-            if ($this instanceof ResolvableProviderInterface) {
-                return $resolver->resolveAndIgnoreUnresolvedVariables($this->getResolvable());
-            }
+            return $resolver->resolveAndIgnoreUnresolvedVariables($this);
         }
 
         return (string) $this;
